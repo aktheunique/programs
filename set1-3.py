4e4d18a044ff53493ddf9ddfaa2c83bb8d83b116
aktheunique <52488901+aktheunique@users.noreply.github.com>
2019-07-09 20:56:21 +0530

    Update set1-3.py

--- set1-3.py
+++ set1-3.py
@@ -1,7 +1,11 @@
-l=['a','e','i','o','u','A','E','I','O','U']
 s=input()
- 
-if s in l:
-	print("Vowel")
+l=['a','e','i','o','u','A','E','I','O','U']
+if((int(ord(s))>=65 and int(ord(s))<=90) or(int(ord(s))>=97 and int(ord(s))<=122)):
+	if s in l:
+		print("Vowel")
+	else:
+		print("Consonant")
+elif((int(ord(s))>=48 and int(ord(s))<=57)):
+	pass
 else:
-	print("Consonant")
+	print("invalid")
