fcbed796b46ba6276d36ebd28593423baefc3e03
aktheunique <52488901+aktheunique@users.noreply.github.com>
2019-07-14 22:23:22 +0530

    Update Pset2-10.py

--- Pset2-10.py
+++ Pset2-10.py
@@ -1,5 +1,12 @@
 string=input()
 l=[]
 for char in string:
-    l.append(chr(ord(char)+3))        #character to ascii and ascii to character 
+    if char=='X':
+        l.append('A')
+    elif char=='Y':
+        l.append('B')
+    elif char=='Z':
+        l.append('C')
+    else:
+        l.append(chr(ord(char)+3))             #character to ascii and viceversa
 print("".join(l))
