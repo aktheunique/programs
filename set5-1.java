fcd8142b8e33d22f4e7f5901ebb2f53115892837
aktheunique <52488901+aktheunique@users.noreply.github.com>
2019-08-10 23:40:36 +0530

    Create set5-1.java

---
+++ set5-1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class set5-1
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    String str=sc.nextLine();
+	    int number=sc.nextInt();
+	    for(int i=0;i<number;i++){
+	        System.out.println(str);
+	    }
+	    
+	}
+}
