071964bf30950f418a5e1b5b1152dbf7d47b6063
aktheunique <52488901+aktheunique@users.noreply.github.com>
2019-08-11 10:29:15 +0530

    Update Begin42.java

--- Begin42.java
+++ Begin42.java
@@ -3,8 +3,8 @@ public class Begin42
 {
 	public static void main(String[] args) {
 	    Scanner sc=new Scanner(System.in);
-	    String s1=sc.nextLine();
-	    String s2=sc.nextLine();
+	    String s1=sc.next();
+	    String s2=sc.next();
 		if(s1.compareTo(s2)>0){
 		    System.out.println(s1);
 		}
