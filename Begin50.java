4b94334fef6b5de703790d4d681b49b6b2e89635
aktheunique <52488901+aktheunique@users.noreply.github.com>
2019-08-11 11:27:51 +0530

    Update Begin50.java

--- Begin50.java
+++ Begin50.java
@@ -4,6 +4,6 @@ public class Begin50
 	public static void main(String[] args) {
 	    Scanner sc=new Scanner(System.in);
 	    int number=sc.nextInt();
-	    System.out.println(number%2==0?"yes":"no");
+	    System.out.println(number%2==0||number==1?"yes":"no");
 	}
 }
