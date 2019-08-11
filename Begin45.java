5ab0d26b23c4e5191abab9d14386a42dcfcd6141
aktheunique <52488901+aktheunique@users.noreply.github.com>
2019-08-11 10:54:09 +0530

    Create Begin45.java

---
+++ Begin45.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Begin45
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int number=sc.nextInt();
+	    int length=String.valueOf(number).length();
+	    System.out.println(length);
+	}
+}
