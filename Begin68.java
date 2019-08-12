41a2e954338f7ca24cbad11152418a129848773a
aktheunique <52488901+aktheunique@users.noreply.github.com>
2019-08-12 09:57:31 +0530

    Create Begin68.java

---
+++ Begin68.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Begin68
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int n1=sc.nextInt();
+		System.out.println(n1%7==0?"yes":"no");
+	}
+}
