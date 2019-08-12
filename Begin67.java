d818a32f270aa5ea2ac1c1a52c8afeccc1a59526
aktheunique <52488901+aktheunique@users.noreply.github.com>
2019-08-12 09:55:14 +0530

    Create Begin67.java

---
+++ Begin67.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Begin67
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int n1=sc.nextInt();
+	    int a=(n1/10)*10;
+	    int f=a+10;
+		System.out.println(f);
+	}
+}
